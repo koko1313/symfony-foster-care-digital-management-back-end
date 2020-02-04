7ff73ffcdeb4768efafd7cd8f7a381b53a2812b3
koko1313 <kaloyanvellichkov@gmail.com>
2020-02-05 00:45:19 +0200

    user delete endpoint added

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -33,9 +36,38 @@ class UsersController extends AbstractController {
     public function getAllUsers(SerializerInterface $serializer) {
         $allUsers = $this->getDoctrine()->getRepository(User::class)->findAll();
 
-        $allUsersJson = $serializer->serialize($allUsers, 'json');
+        $context = new SerializationContext();
+        $context->setSerializeNull(true); // serialize null values too
+
+        $allUsersJson = $serializer->serialize($allUsers, 'json', $context);
 
         return new Response($allUsersJson);
+    }
+
+
+    /**
+     * @Route("/user/delete", methods={"DELETE"})
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function deleteUser(Request $req, EntityManagerInterface $entityManager) {
+        $userId = $req->get("userId");
+
+        $user = $this->getDoctrine()->getRepository(User::class)->find($userId);
+
+        if(!$user) {
+            return new Response(null, Response::HTTP_BAD_REQUEST);
+        }
+
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        // check if user was deleted
+        $user = $this->getDoctrine()->getRepository(User::class)->find($userId);
+        if($user) {
+            return new Response(null, Response::HTTP_BAD_REQUEST);
+        }
+
+        return new Response(null);
     }
 
 }
