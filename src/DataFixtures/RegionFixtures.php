751979e50dd215783dec2aaba57117b0f3b59213
koko1313 <kaloyanvellichkov@gmail.com>
2020-02-04 17:07:09 +0200

    added region fixtures

---
+++ src/DataFixtures/RegionFixtures.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Region;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class RegionFixtures extends Fixture {
+    public function load(ObjectManager $manager) {
+        $regions = [
+            "Благоевград",
+            "Бургас",
+            "Варна",
+            "Велико Търново",
+            "Видин",
+            "Граца",
+            "Габрово",
+            "Добрич",
+            "Кърджали",
+            "Кюстендил",
+            "Ловеч",
+            "Монтана",
+            "Пазарджик",
+            "Перник",
+            "Плевен",
+            "Пловдив",
+            "Разград",
+            "Русе",
+            "Силистра",
+            "Сливен",
+            "Смолян",
+            "София",
+            "Стара Загора",
+            "Търговище",
+            "Хасково",
+            "Шумен",
+            "Ямбол"
+        ];
+
+        foreach($regions as $r) {
+            $region = new Region();
+            $region->setName($r);
+            $manager->persist($region);
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies() {
+        return array(
+            RoleFixtures::class,
+        );
+    }
+}
