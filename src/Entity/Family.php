536a9de23f6471c12425d27b2d90f9ff01fa5080
koko1313 <kaloyanvellichkov@gmail.com>
2020-02-06 17:24:34 +0200

    created family entity

---
+++ src/Entity/Family.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\FamilyRepository")
+ */
+class Family {
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titular;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $womanFirstName;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $womanSecondName;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $manFirstName;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $manSecondName;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $manLastName;
+
+    /**
+     * @ORM\Column(type="string", length=1, nullable=true)
+     */
+    private $preferKidGender;
+
+    /**
+     * @ORM\Column(type="smallint", nullable=true)
+     */
+    private $preferKidMinAge;
+
+    /**
+     * @ORM\Column(type="smallint", nullable=true)
+     */
+    private $preferKidMaxAge;
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getTitular() {
+        return $this->titular;
+    }
+
+    public function setTitular(string $titular) {
+        $this->titular = $titular;
+        return $this;
+    }
+
+    public function getWomanFirstName() {
+        return $this->womanFirstName;
+    }
+
+    public function setWomanFirstName(string $womanFirstName) {
+        $this->womanFirstName = $womanFirstName;
+        return $this;
+    }
+
+    public function getWomanSecondName() {
+        return $this->womanSecondName;
+    }
+
+    public function setWomanSecondName(string $womanSecondName) {
+        $this->womanSecondName = $womanSecondName;
+        return $this;
+    }
+
+    public function getManFirstName() {
+        return $this->manFirstName;
+    }
+
+    public function setManFirstName(string $manFirstName) {
+        $this->manFirstName = $manFirstName;
+        return $this;
+    }
+
+    public function getManSecondName() {
+        return $this->manSecondName;
+    }
+
+    public function setManSecondName(string $manSecondName) {
+        $this->manSecondName = $manSecondName;
+        return $this;
+    }
+
+    public function getManLastName() {
+        return $this->manLastName;
+    }
+
+    public function setManLastName(string $manLastName) {
+        $this->manLastName = $manLastName;
+        return $this;
+    }
+
+    public function getPreferKidGender() {
+        return $this->preferKidGender;
+    }
+
+    public function setPreferKidGender(?string $preferKidGender) {
+        $this->preferKidGender = $preferKidGender;
+        return $this;
+    }
+
+    public function getPreferKidMinAge() {
+        return $this->preferKidMinAge;
+    }
+
+    public function setPreferKidMinAge(?int $preferKidMinAge) {
+        $this->preferKidMinAge = $preferKidMinAge;
+        return $this;
+    }
+
+    public function getPreferKidMaxAge() {
+        return $this->preferKidMaxAge;
+    }
+
+    public function setPreferKidMaxAge(?int $preferKidMaxAge) {
+        $this->preferKidMaxAge = $preferKidMaxAge;
+        return $this;
+    }
+}
---
+++ src/Migrations/Version20200206152259.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200206152259 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE family (id INT AUTO_INCREMENT NOT NULL, titular VARCHAR(255) NOT NULL, woman_first_name VARCHAR(50) NOT NULL, woman_second_name VARCHAR(50) NOT NULL, man_first_name VARCHAR(50) NOT NULL, man_second_name VARCHAR(50) NOT NULL, man_last_name VARCHAR(50) NOT NULL, prefer_kid_gender VARCHAR(1) DEFAULT NULL, prefer_kid_min_age SMALLINT DEFAULT NULL, prefer_kid_max_age SMALLINT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE family');
+    }
+}
