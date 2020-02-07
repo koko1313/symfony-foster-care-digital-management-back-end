3500d49467671c358db89d29f2c58c188ff804e4
koko1313 <kaloyanvellichkov@gmail.com>
2020-02-08 01:20:54 +0200

    refactor

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -26,14 +26,17 @@ class SecurityController extends AbstractController {
      * @Route("/login", methods={"POST"})
      */
     public function login(Request $req, SerializerInterface $serializer, EntityManagerInterface $entityManager) {
-        $user = $entityManager->getRepository(User::class)->findOneBy(["email" => $req->get("email")]);
+        $email = $req->get("email");
+        $password = $req->get("password");
+
+        $user = $entityManager->getRepository(User::class)->findOneBy(["email" => $email]);
         $entityManager->close();
 
         if($user) {
             // get the concrete user, based on $user class name - get_class($user)
-            $concreteUser = $entityManager->getRepository(get_class($user))->findOneBy(["email" => $req->get("email")]);
+            $concreteUser = $entityManager->getRepository(get_class($user))->findOneBy(["email" => $email]);
 
-            if($req->get("password") && $this->encoder->isPasswordValid($concreteUser, $req->get("password"))) {
+            if($password && $this->encoder->isPasswordValid($concreteUser, $password)) {
                 // Manually authenticate user in controller
                 $token = new UsernamePasswordToken($concreteUser, $concreteUser->getPassword(), 'main', $concreteUser->getRoles());
                 $this->get('security.token_storage')->setToken($token);
