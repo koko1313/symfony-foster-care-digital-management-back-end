8f0e46b764f93e21045c2a25f1f7a5d477c2946b
koko1313 <kaloyanvellichkov@gmail.com>
2020-02-08 21:28:59 +0200

    deployment settings

--- src/Migrations/Version20200208192606.php
+++ src/Migrations/Version20200208192606.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200208155418 extends AbstractMigration
+final class Version20200208192606 extends AbstractMigration
 {
     public function getDescription() : string
     {
